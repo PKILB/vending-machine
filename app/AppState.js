6908d08169fe845ff95ab1b5a5f971217dbb33a7
Payton Boone <paytonboone30@gmail.com>
2023-01-31 14:52:24 -0700

    Initial array added and working

--- app/AppState.js
+++ app/AppState.js
@@ -1,3 +1,4 @@
+import { Snack } from "./Models/Snack.js"
 import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
@@ -6,7 +7,20 @@ import { loadState } from "./Utils/Store.js"
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
   values = loadState('values', [Value])
+
+  /** @type {import('./Models/Snack').Snack[]} */
+  snacks = [
+    new Snack(
+    {
+      name:'Rainbow-Sherbert Reign', 
+      price: 3.50, 
+      imgUrl: 'https://s7.vitaminshoppe.com/is/image/VitaminShoppe/2237204_01?$OP_PDPZOOM$'
+    }
+    ),
+      ]
 }
+    
+    
 
 export const appState = new Proxy(new AppState(), {
   get(target, prop) {
